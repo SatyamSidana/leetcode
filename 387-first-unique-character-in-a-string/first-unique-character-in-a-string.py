a0d5fbc535061385916b1795ad416df6a988f57d
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2023-10-13 11:53:17 +0530

    Time: 148 ms (24.53%) | Memory: 16.4 MB (92.98%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -1,13 +1,15 @@
-from collections import defaultdict as dd
 class Solution:
-    def getFreq(self, s):
-        freq = dd(lambda: 0)
-        for c in s:
-            freq[c] += 1
-        return freq
     def firstUniqChar(self, s: str) -> int:
-        freq = self.getFreq(s)
-        for c in freq:
-            if freq[c] == 1:
-                return s.index(c)
+        a={}
+        for i in range (len(s)):
+            if s[i] in a:
+                a.update({s[i]:-1})
+            else:
+                a[s[i]]=i
+        b=list(a.values())
+        for i in b:
+            if i!=-1:
+                return i
+
         return -1
+        
