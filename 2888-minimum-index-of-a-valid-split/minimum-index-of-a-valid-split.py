21482388a241118bf0333e5622f57e1301f81cd7
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2023-10-27 08:56:28 +0530

    Time: 673 ms (21.48%) | Memory: 31.8 MB (64.08%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.py
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.py
@@ -1,8 +1,9 @@
 class Solution:
     def minimumIndex(self, n: List[int]) -> int:
         a={}
+        b=len(n)
         s=0
-        for i in range (len(n)):
+        for i in range (b):
             if n[i] in a:
                 a[n[i]]=a.get(n[i])+1
             else:
@@ -10,11 +11,11 @@ class Solution:
             if a.get(n[i])*2>len(n)+1:
                 s=n[i]
         l,r=0,a.get(s)
-        for i in range (len(n)):
+        for i in range (b):
             if n[i]==s:
                 l+=1
                 r-=1
-            if l*2>i+1 and r*2>len(n)-i-1:
+            if l*2>i+1 and r*2>b-i-1:
                 return i    
         
         return -1
