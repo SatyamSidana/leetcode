a92c694a0026ef8e36fdb0d6973b4a5d629a1d89
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2023-11-05 16:14:11 +0530

    Time: 1321 ms (56.99%) | Memory: 33.9 MB (94.39%) - LeetSync

--- 2211-k-radius-subarray-averages/k-radius-subarray-averages.py
+++ 2211-k-radius-subarray-averages/k-radius-subarray-averages.py
@@ -1,46 +1,17 @@
 class Solution:
     def getAverages(self, nums: List[int], k: int) -> List[int]:
-        # Get the length of the nums list
-        n = len(nums)
+        res, left, windowSum, windowLength = [-1] * len(nums), 0, 0, 2 * k + 1
         
-        # Handle edge cases
-        if n == 1:
-            if k < n:
-                return nums
-            return [-1]
+        for right in range(len(nums)):
+            windowSum += nums[right]  # Add the current element to the window sum
+            
+            # Check if the window length is reached
+            if right - left + 1 == windowLength:
+                # Calculate the average of the window and store it in the result array
+                res[left + k] = windowSum // windowLength
+                
+                # Adjust the window sum and move the left pointer
+                windowSum -= nums[left]
+                left += 1
         
-        if k > n:
-            return [-1] * n
-        
-        # Initialize prefix sum array
-        psum = [0] * n
-        psum[0] = nums[0]
-        
-        # Calculate the prefix sum
-        for i in range(1, n):
-            psum[i] = psum[i - 1] + nums[i]
-        
-        # Define the lower and upper limits for calculating averages
-        L = k
-        H = n - k
-        
-        # Initialize the answer array with -1
-        ans = [-1] * n
-        
-        # Calculate the range of values for averaging
-        r = 2 * k + 1
-        
-        # If the range is larger than the total number of elements, return the answer array
-        if r > n:
-            return ans
-        
-        # Calculate the average for the first element
-        ans[L] = psum[L + k] // r
-        
-        c = 0
-        # Calculate the averages for the remaining elements
-        for i in range(L + 1, H):
-            ans[i] = (psum[i + k] - psum[c]) // r
-            c += 1
-        
-        return ans
+        return res
