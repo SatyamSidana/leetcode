c84fa601240eca9c70a4a2a24ed05f62d4256877
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2023-11-10 10:58:59 +0530

    Time: 50 ms (36.81%) | Memory: 16.8 MB (23.40%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        b={}
+        for i in range (len(s)):
+            if b.get(t[i]):
+                if s[i]!=b[t[i]]:
+                    return False
+            else:
+                print(b)
+                for j in range (len(b)):
+                    if s[i]==b[t[j]]:
+                        return False
+                b[t[i]]=s[i]
+        print(b)    
+        return True
