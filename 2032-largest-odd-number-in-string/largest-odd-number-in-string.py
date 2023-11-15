ed0f1bcd87b102db5463afb2049d5c620a00dc8d
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2023-11-15 10:01:11 +0530

    Time: 720 ms (5.03%) | Memory: 18.6 MB (5.37%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.py
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.py
@@ -3,9 +3,9 @@ class Solution:
         m=""
         a=""
         b=["1","3","5","7","9"]
-        for i in range (len(n)):
-            a+=n[i]
-            if n[i] in b:
+        for i in n:
+            a+=i
+            if i in b:
                 m=a
         return m
         
