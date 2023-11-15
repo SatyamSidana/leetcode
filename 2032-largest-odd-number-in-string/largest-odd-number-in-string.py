a6bb5c8dde0491ca5edb536f76f597fd58adcf4f
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2023-11-15 10:04:54 +0530

    Time: 115 ms (11.28%) | Memory: 17.9 MB (34.97%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.py
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.py
@@ -1,9 +1,9 @@
 class Solution:
     def largestOddNumber(self, n: str) -> str:
         m=-1
-        b=["1","3","5","7","9"]
+        b={"1":1,"3":1,"5":1,"7":1,"9":1}
         for i in range (len(n)):
-            if n[i] in b:
+            if b.get(n[i]):
                 m=i
         if m==-1:
             return ""
