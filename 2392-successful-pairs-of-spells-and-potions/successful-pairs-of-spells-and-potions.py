b21e47c50bf8f4652e76095c46cc76aa2a31079b
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2023-11-15 14:26:52 +0530

    Time: 1447 ms (56.93%) | Memory: 39.8 MB (27.23%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.py
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.py
@@ -7,15 +7,11 @@ class Solution:
             l=0
             h=b-1
             t=s/sp[i]
-            print(t)
             while(l<=h):
                 mid=(l+h)//2
                 if p[mid]<t:
                     l=mid+1
-                elif p[mid]>t:
-                    h=mid-1
                 else:
-                    lh=l
                     h=mid-1
             a[i]=b-l
         return a
