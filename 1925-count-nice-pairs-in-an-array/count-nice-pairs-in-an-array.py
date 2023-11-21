76a85f171bdf903b0f4ffc2979e74dbb4ca6d6d2
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2023-11-21 09:54:26 +0530

    Time: 866 ms (5.10%) | Memory: 26.8 MB (33.99%) - LeetSync

--- 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.py
+++ 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.py
@@ -7,12 +7,10 @@ class Solution:
                 b = b* 10 + digit
                 a //= 10
             return b
-        d={}
+        d=defaultdict(int)
         c=0
         for i in n:
-            if d.get((i-rev(i))):
-                c+=d[(i-rev(i))]
-                d[(i-rev(i))]+=1
-            else:
-                d[(i-rev(i))]=1
+            
+            c+=d[(i-rev(i))]
+            d[(i-rev(i))]+=1
         return c%((10**9) + 7)
