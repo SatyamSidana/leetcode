48a9937dcfe0e4ec7667825e987b88c8bbd47169
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2023-12-26 13:10:40 +0530

    Time: 54 ms (84.70%) | Memory: 17.6 MB (6.44%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -8,20 +8,15 @@ class Solution:
             if s[e] in a:
                 if a[s[e]]<st:
                     m=max(m,e-st+1)
-                    print(st,e)
                 else:
                     z=a[s[e]]
-                    print("z",s[e],z)
                     st=z+1
-                a[s[e]]=e
-                e+=1
             else:
-                a[s[e]]=e
-                print(a)
-                print(st,e)
+            
                 
                 m=max(m,e-st+1)
-                e+=1
+            a[s[e]]=e
+            e+=1
         return m
             
 
