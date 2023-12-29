4e63eb890385d88319151ea33dacf8b56fb11430
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2023-12-29 22:03:51 +0530

    Time: 61 ms (8.54%) | Memory: 17.3 MB (5.16%) - LeetSync

---
+++ 2536-determine-if-two-events-have-conflict/determine-if-two-events-have-conflict.py
@@ -0,0 +1,7 @@
+class Solution:
+    def haveConflict(self, event1: List[str], event2: List[str]) -> bool:
+        start1 = int(event1[0][:2]) * 60 + int(event1[0][3:])
+        end1 = int(event1[1][:2]) * 60 + int(event1[1][3:])
+        start2 = int(event2[0][:2]) * 60 + int(event2[0][3:])
+        end2 = int(event2[1][:2]) * 60 + int(event2[1][3:])
+        return True if start1 <= start2 <= end1 or start2 <= start1 <= end2 else False
