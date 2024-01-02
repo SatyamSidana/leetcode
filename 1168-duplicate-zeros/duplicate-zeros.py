7e78b66494eb0cd6456d266f25ab043630a72775
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-01-02 23:29:07 +0530

    Time: 133 ms (16.46%) | Memory: 18.3 MB (5.08%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.py
+++ 1168-duplicate-zeros/duplicate-zeros.py
@@ -5,8 +5,8 @@ class Solution:
             if a[i]==0:
                 a.insert(i+1,0)
                 a.pop()
-                i+=2
-            else:
                 i+=1
+            
+            i+=1
         
         
