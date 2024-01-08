50d1e418a1daccbfb1ababc2a65c77d86179ebba
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-01-08 13:34:46 +0530

    Time: 45 ms (73.61%) | Memory: 17.4 MB (27.16%) - LeetSync

---
+++ 682-baseball-game/baseball-game.py
@@ -0,0 +1,17 @@
+class Solution:
+    def calPoints(self, o: List[str]) -> int:
+        a=[]
+        for i in o:
+            if i=="C" and len(a)>0:
+                a.pop()
+            elif i=="D" :
+                a.append(a[-1]*2)
+            elif i=="+":
+                a.append(a[-1]+a[-2])
+            else:
+                a.append(int(i))
+        return sum(a)
+
+
+
+        
