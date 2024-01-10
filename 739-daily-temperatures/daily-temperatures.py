de9c95ffd80c2aff6e7eee27e350eefb83f07c76
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-01-11 00:01:34 +0530

    Time: 887 ms (94.28%) | Memory: 31.5 MB (52.03%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,28 +1,13 @@
 class Solution:
-    def dailyTemperatures(self, t: List[int]) -> List[int]:
-        m={}
-        a=[]
-        s=[]
-        s.append((t[0],0))
-        for i in range (1,len(t)):
-            while s and t[i]>s[-1][0]:
-                if s[-1][0] in m:
-                    m[s[-1][0]][s[-1][1]]=i-s[-1][1]
-                else:
-                    m[s[-1][0]]={}
-                    m[s[-1][0]][s[-1][1]]=i-s[-1][1]
-                s.pop()
-            s.append((t[i],i))
-        for i in range (len(s)):
-            if s[i][0] in m:
-                m[s[i][0]][s[i][1]]=0
-            else:
-                m[s[i][0]]={}
-                m[s[i][0]][s[i][1]]=0
-        for i in range (len(t)):
-            a.append(m[t[i]][i])
-        return a
-
-                
-
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        answer = [0] * len(temperatures)
+        stack = []
         
+        for i in range(len(temperatures)):
+            while len(stack) > 0 and temperatures[i] > temperatures[stack[-1]]:
+                prev_index = stack.pop()
+                answer[prev_index] = i - prev_index
+
+            stack.append(i)
+
+        return answer
