40904bc7a828e199f79d32e04de417bdb163198c
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-01-16 12:24:15 +0530

    Time: 217 ms (58.50%) | Memory: 18.7 MB (34.29%) - LeetSync

--- 495-teemo-attacking/teemo-attacking.py
+++ 495-teemo-attacking/teemo-attacking.py
@@ -1,9 +1,8 @@
 class Solution:
     def findPoisonedDuration(self, t: List[int], d: int) -> int:
-        m=0
+        m=d
         for i in range (len(t)-1):
             m+=min(d,t[i+1]-t[i])
-        m+=d
         return m
 
 
