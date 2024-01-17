9a849288de6966a9d16e4f673cc69d5fc1b77461
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-01-17 13:14:11 +0530

    Time: 48 ms (32.51%) | Memory: 17.3 MB (41.60%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -1,16 +1,16 @@
 class Solution:
     def uniqueOccurrences(self, a: List[int]) -> bool:
         b={}
-        c={}
+        c=set()
         for i in a:
             if b.get(i):
                 b[i]+=1
             else:
                 b[i]=1
         for i in b:
-            if c.get(b[i]):
-                return False
-            c[b[i]]=1
+            c.add(b[i])
+        if len(b)!=len(c):
+            return False
         return True
 
 
