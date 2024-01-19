489e11970c34090202e7cd2a06d1485214e0586a
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-01-19 10:45:32 +0530

    Time: 52 ms (16.79%) | Memory: 17.4 MB (39.59%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.py
@@ -0,0 +1,24 @@
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+        m=""
+        a=[]
+        a.append(s[0])
+        c=1
+        for i in range (1,len(s)):
+            if s[i]==")":
+                c-=1
+            else:
+                c+=1
+            a.append(s[i])
+            if c==0:
+                a.pop()
+                a.pop(0)
+                b="".join(a)
+                m+=b
+                a=[]
+        return m
+
+
+
+        
+        
