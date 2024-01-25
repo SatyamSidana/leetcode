ce6efdfcc26f233dbeee4f4d32539df9aad97100
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-01-25 12:38:39 +0530

    Time: 30 ms (95.11%) | Memory: 16.7 MB (98.20%) - LeetSync

---
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.py
@@ -0,0 +1,16 @@
+class Solution:
+    def halvesAreAlike(self, n: str) -> bool:
+        a={"a":1,"e":1,"i":1,"o":1,"u":1,"A":1,"E":1,"I":1,"O":1,"U":1}
+        c=0
+        for i in range (len(n)//2):
+            if a.get(n[i]):
+                c+=1
+        print(c)
+        for i in range (len(n)//2,len(n)):
+            if a.get(n[i]):
+                c-=1
+        if c==0:
+            return True
+        return False
+
+        
