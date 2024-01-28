6e7d7b06a7b4f97d94e7fbddb240746f08aa71d6
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-01-28 17:47:33 +0530

    Time: 62 ms (87.66%) | Memory: 16.7 MB (62.34%) - LeetSync

--- 983-validate-stack-sequences/validate-stack-sequences.py
+++ 983-validate-stack-sequences/validate-stack-sequences.py
@@ -5,7 +5,6 @@ class Solution:
         j=0
         for i in range (len(po)):
             if po[i] in a:
-                print(po[i]) 
                 if st[-1]!=po[i]:
                     return False
                 else:
@@ -19,6 +18,5 @@ class Solution:
                         st.append(p[j])
                         a[p[j]]=0
                     j+=1
-            print(st,a)
         return True
         
