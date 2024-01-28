518689a2db92dc6cef3ec06c6d8a21fa65f28431
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-01-28 17:17:26 +0530

    Time: 40 ms (36.20%) | Memory: 16.6 MB (56.95%) - LeetSync

---
+++ 886-score-of-parentheses/score-of-parentheses.py
@@ -0,0 +1,27 @@
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        c=0
+        m=0
+        st=[]
+        for i in s:
+            if i=="(":
+                m+=1
+                if m-1<len(st):
+                    pass
+                else:
+                    st.append(0)
+            elif i==")" and st:
+                if m<len(st):
+                    st[m-1]+=2*st[m]
+                    st.pop()
+                else:
+                    st[m-1]+=1
+                m-=1
+            print(st)
+            if m==0:
+                c+=st[0]
+                st=[]
+            
+        
+        return c
+        
