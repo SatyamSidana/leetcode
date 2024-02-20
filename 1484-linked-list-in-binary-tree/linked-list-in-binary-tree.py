8f1346406125fbb6a6d818cdad4bef5eefe940af
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-02-20 09:40:27 +0530

    Time: 180 ms (8.44%) | Memory: 17.3 MB (69.06%) - LeetSync

---
+++ 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.py
@@ -0,0 +1,47 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
+        a=[]
+        b=[]
+        def check(a,pt):
+            i=0
+            ptr=pt
+            while i <len(a):
+                if a[i]==ptr.val:
+                    j=i
+                    i+=1
+                    while j<len(a) and ptr and a[j]==ptr.val:
+                        j+=1
+                        ptr=ptr.next    
+                    if ptr==None:
+                        return True
+                    else:
+                        ptr=pt
+                else:
+                    i+=1
+            return False
+        def preorder(cur):
+            if cur!=None:
+                a.append(cur.val)
+                preorder(cur.left)
+                preorder(cur.right)
+                if cur.left==None and cur.right==None:
+                    if check(list(a),head):
+                        b.append(1)
+                a.pop()
+        preorder(root)
+        if len(b)>0:
+            return True
+        return False
+                    
+        
