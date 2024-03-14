ae273dbe8f342bbf077626236d2eb374bc70d388
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-03-14 22:55:20 +0530

    Time: 240 ms (8.68%) | Memory: 20.5 MB (28.42%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numSubarraysWithSum(self, n: List[int], goal: int) -> int:
+        p=[]
+        p.append(n[0])
+        c=0
+        for i in range (len(n)-1):
+            p.append(p[i]+n[i+1])
+        print(p)
+        a={}
+        a[0]=1
+        for i in p:
+            if a.get(i-goal):
+                c+=a[i-goal]
+            if i in a:
+                a[i]+=1
+            else:
+                a[i]=1
+        return c
+
+
