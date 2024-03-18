30d7fdf22c30f5ab8d9275296a55e6d96fc5e290
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-03-18 07:45:49 +0530

    Time: 144 ms (6.06%) | Memory: 16.6 MB (13.45%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def sum(a):
+            c=0
+            while a!=0:
+                c+=(a%10)**2
+                a=a//10
+            return c
+        i=1000
+        while i>0:
+            if sum(n)==1:
+                return True
+            n=sum(n)
+            i-=1 
+        return False     
+        
