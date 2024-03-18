f740306ced3fbc5db3bb407f768aa62f86adde3a
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-03-18 21:47:31 +0530

    Time: 192 ms (31.37%) | Memory: 17.3 MB (61.74%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findCircleNum(self, c: List[List[int]]) -> int:
+        p=0
+        v=[0]*len(c)
+        e=[]
+        for i in range (len(c)):
+            if v[i]==1:
+                pass
+            else:
+                e.append(i)
+                v[i]=1
+                while e:
+                    b=e.pop(0)
+                    for j in range (len(c[0])):
+                        if c[b][j]==1 and v[j]==0:
+                            v[j]=1
+                            e.append(j)
+                p+=1
+        return p
+
+                
+        
+        
