398cd20c4932af79087220ba2a2606005c041177
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-03-23 18:13:29 +0530

    Time: 52 ms (56.68%) | Memory: 16.6 MB (77.53%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
@@ -4,9 +4,7 @@ class Solution:
         b=2
         while(b<len(n)):
             if n[a]==n[b]:
-                while n[a]==n[b] and b<len(n)-1:
-                    n.pop(b)
-                if b==len(n)-1 and n[a]==n[b]:
+                while  b<len(n) and n[a]==n[b] :
                     n.pop(b)
             a+=1
             b+=1
