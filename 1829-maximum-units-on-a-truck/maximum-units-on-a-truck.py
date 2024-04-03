b4aaa9710a6d2c55967a824d71c556011f9767c0
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-04-03 12:50:19 +0530

    Time: 142 ms (37.88%) | Memory: 17 MB (84.72%) - LeetSync

---
+++ 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maximumUnits(self, b: List[List[int]], t: int) -> int:
+        def swap(s):
+            temp=s[0]
+            s[0]=s[1]
+            s[1]=temp
+        c=0
+        for i in b:
+            swap(i)
+        b.sort(reverse=True)
+        print(b)
+        for i in b:
+            if i[1]<t:
+                c+=i[0]*i[1]
+                t-=i[1]
+            else:
+                c+=i[0]*t
+                break
+        return c
