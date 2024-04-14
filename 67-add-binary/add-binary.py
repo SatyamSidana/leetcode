9c491ba6159c9d0f4b089c923d8c458d9b007785
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-04-15 01:29:50 +0530

    Time: 44 ms (15.68%) | Memory: 16.7 MB (35.10%) - LeetSync

---
+++ 67-add-binary/add-binary.py
@@ -0,0 +1,37 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        c=""
+        r=0
+        i=len(a)-1
+        j=len(b)-1
+        while i>=0 and j>=0:
+            if int(a[i])+int(b[j])+r>1:
+                c+=(str(int(a[i])+int(b[j])+r-2))
+                r=1
+            else:
+                c+=str(int(a[i])+int(b[j])+r)
+                r=0
+            i-=1
+            j-=1
+            print(c)
+        while i>=0:
+            if int(a[i])+r>1:
+                c+=(str(int(a[i])+r-2))
+                r=1
+            else:
+                c+=str(int(a[i])+r)
+                r=0
+            i-=1
+        while j>=0:
+            if int(b[j])+r>1:
+                c+=(str(int(b[j])+r-2))
+                r=1
+            else:
+                c+=str(+int(b[j])+r)
+                r=0
+            j-=1
+        if r:
+            c+="1"
+        return c[::-1]
+
+        
