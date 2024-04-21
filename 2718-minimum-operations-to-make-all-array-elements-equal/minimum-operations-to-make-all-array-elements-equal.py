61e2750124513a65aff59aea98d9c44ab927c806
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-04-21 14:40:02 +0530

    Time: 981 ms (9.69%) | Memory: 46.1 MB (21.32%) - LeetSync

---
+++ 2718-minimum-operations-to-make-all-array-elements-equal/minimum-operations-to-make-all-array-elements-equal.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minOperations(self, n: List[int], q: List[int]) -> List[int]:
+        a=[]
+        p=[]
+        n.sort()
+        p.append(n[0])
+        for i in range (1,len(n)):
+            p.append(n[i]+p[i-1])
+        print(p)
+        for i in q:
+            l=0
+            h=len(n)-1
+            while l<=h:
+                mid=(l+h)//2
+                if n[mid]<i:
+                    l=mid+1
+                elif n[mid]>i:
+                    h=mid-1
+                else:
+                    h=mid
+                    l=mid+1
+            print(h)
+            if h<0:
+                h=0
+            a.append(abs(p[h]-(i*(h+1)))+abs((p[-1]-p[h]-(i*(len(p)-h-1)))))
+        return a
+        
