75292a872728497212e910b69db2aa44bc5ed230
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-04-21 14:40:24 +0530

    Time: 937 ms (17.83%) | Memory: 44.4 MB (51.55%) - LeetSync

--- 2718-minimum-operations-to-make-all-array-elements-equal/minimum-operations-to-make-all-array-elements-equal.py
+++ 2718-minimum-operations-to-make-all-array-elements-equal/minimum-operations-to-make-all-array-elements-equal.py
@@ -6,7 +6,6 @@ class Solution:
         p.append(n[0])
         for i in range (1,len(n)):
             p.append(n[i]+p[i-1])
-        print(p)
         for i in q:
             l=0
             h=len(n)-1
@@ -19,7 +18,6 @@ class Solution:
                 else:
                     h=mid
                     l=mid+1
-            print(h)
             if h<0:
                 h=0
             a.append(abs(p[h]-(i*(h+1)))+abs((p[-1]-p[h]-(i*(len(p)-h-1)))))
