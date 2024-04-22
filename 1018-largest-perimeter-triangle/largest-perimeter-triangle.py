872893c5ba29b6e1927408df88b7bcaa8518ae68
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-04-23 01:11:31 +0530

    Time: 154 ms (77.13%) | Memory: 17.8 MB (94.43%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.py
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.py
@@ -3,10 +3,12 @@ class Solution:
         n.sort()
         c=0
         for i in range (2,len(n)):
-            h=i-1
-            l=i-2
-            if n[h]+n[l]>n[i]:
-                c=max(c,n[h]+n[l]+n[i])
+            j=len(n)-i+1
+            h=j-1
+            l=j-2
+            if n[h]+n[l]>n[j]:
+                c=max(c,n[h]+n[l]+n[j])
+                break
         return c
                 
 
