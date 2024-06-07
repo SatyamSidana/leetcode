c0f523f4ce70e2c7cd18d362ec8a9784c5c6f236
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-06-07 22:24:56 +0530

    Time: 395 ms (10.77%) | Memory: 26 MB (63.80%) - LeetSync

--- 648-replace-words/replace-words.py
+++ 648-replace-words/replace-words.py
@@ -4,6 +4,7 @@ class Solution:
         for i in d:
             a[i]=1
         s=list(s.split())
+        c=""
         for i in range (len(s)):
             j=0
             while j<len(s[i]):
@@ -11,9 +12,6 @@ class Solution:
                     s[i]=s[i][:j]
                     break
                 j+=1
-        c=""
-        for i in range (len(s)-1):
             c+=s[i]
             c+=" "
-        c+=s[-1]
-        return c
+        return c[:len(c)-1]
