f293802449567d4957ca297fb261c2ea01774ce2
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-07-05 18:44:15 +0530

    Time: 70 ms (15.68%) | Memory: 18 MB (19.11%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -4,17 +4,17 @@ class Solution:
         
         dp=[]
         for i in range (len(t)):
-            dp.append([-(10^5)]*(i+1))
-        dp.append([(-(10^5))]*(len(t)+1))
+            dp.append([-1]*(i+1))
+        dp.append([(-1)]*(len(t)+1))
         def calc(x,y):
             if x==len(t)-1:
                 dp[x][y]=t[x][y]
                 return t[x][y]
-            if dp[x+1][y+1]!=-(10^5):
+            if dp[x+1][y+1]!=-1:
                 a=dp[x+1][y+1]
             else:
                 a=calc(x+1,y+1)
-            if dp[x+1][y]!=-(10^5):
+            if dp[x+1][y]!=-1:
                 b=dp[x+1][y]
             else:
                 b=calc(x+1,y)
