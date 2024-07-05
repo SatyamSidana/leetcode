0f432a3710e55e43af37eae36e32541773c720c8
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-07-05 18:43:32 +0530

    Time: 67 ms (25.38%) | Memory: 18.1 MB (19.11%) - LeetSync

---
+++ 120-triangle/triangle.py
@@ -0,0 +1,25 @@
+class Solution:
+    m=10^9
+    def minimumTotal(self, t: List[List[int]]) -> int:
+        
+        dp=[]
+        for i in range (len(t)):
+            dp.append([-(10^5)]*(i+1))
+        dp.append([(-(10^5))]*(len(t)+1))
+        def calc(x,y):
+            if x==len(t)-1:
+                dp[x][y]=t[x][y]
+                return t[x][y]
+            if dp[x+1][y+1]!=-(10^5):
+                a=dp[x+1][y+1]
+            else:
+                a=calc(x+1,y+1)
+            if dp[x+1][y]!=-(10^5):
+                b=dp[x+1][y]
+            else:
+                b=calc(x+1,y)
+            dp[x][y]=min(a,b)+t[x][y]
+            return (min(a,b)+t[x][y])
+        calc(0,0)
+        return dp[0][0]
+            
