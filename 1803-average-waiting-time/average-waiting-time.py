dbb660f842d10ad31b883e4a140d671a3156d3fe
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-07-09 21:40:18 +0530

    Time: 848 ms (10.65%) | Memory: 57.3 MB (9.97%) - LeetSync

---
+++ 1803-average-waiting-time/average-waiting-time.py
@@ -0,0 +1,13 @@
+class Solution:
+    def averageWaitingTime(self, c: List[List[int]]) -> float:
+        w=0
+        t=0
+        for i in c:
+            if t>i[0]:
+                w+=t-i[0]+i[1]
+                t+=i[1]
+            else:
+                w+=i[1]
+                t=i[0]+i[1]
+            print(w,t)
+        return w/len(c)
