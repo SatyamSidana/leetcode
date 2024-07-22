29553232a4752966fb020033d65834cb8533d3d9
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-07-22 12:33:32 +0530

    Time: 731 ms (95.32%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+Select Students.student_id , Students.student_name , Subjects.subject_name ,count(Examinations.student_id) as
+attended_exams from Students join Subjects 
+left join Examinations on Students.student_id=Examinations.student_id and Subjects.subject_name= Examinations.subject_name
+group by Students.student_id , Students.student_name , Subjects.subject_name ORDER BY Students.student_id, Subjects.subject_name;
+
