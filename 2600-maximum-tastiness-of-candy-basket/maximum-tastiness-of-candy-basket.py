aa2bcac4784054ab9e5f50e01b8a628433bf81fd
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-07-29 17:58:52 +0530

    Time: 911 ms (70.96%) | Memory: 28.6 MB (33.15%) - LeetSync

---
+++ 2600-maximum-tastiness-of-candy-basket/maximum-tastiness-of-candy-basket.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maximumTastiness(self, p: List[int], k: int) -> int:
+        p.sort()
+        def isvalid(num):
+            c=1
+            diff=num+p[0]
+            for i in range (1,len(p)):
+                if p[i]>=diff:
+                    c+=1
+                    diff=num+p[i]
+            return c
+        l=0
+        h=p[-1]-p[0]
+        ans=0
+        while l<=h:
+            mid=(l+h)//2
+            if isvalid(mid)>=k:
+                ans=mid
+                l=mid+1
+            else:
+                h=mid-1
+        print(isvalid(8))
+        return ans
+        
