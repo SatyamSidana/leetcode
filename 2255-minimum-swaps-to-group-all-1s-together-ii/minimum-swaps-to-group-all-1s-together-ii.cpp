28fabaea705ea40bed15eb7f1720e7915a8040aa
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-08-03 00:44:53 +0530

    Time: 52 ms (98.43%) | Memory: 89.8 MB (28.09%) - LeetSync

--- 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     int minSwaps(vector<int>& nums) {
-        int k = accumulate(nums.begin(), nums.end(), 0);
-        int n = nums.size();
-        int cnt = accumulate(nums.begin(), nums.begin() + k, 0);
-        int mx = cnt;
-        for (int i = k; i < n + k; ++i) {
-            cnt += nums[i % n] - nums[(i - k + n) % n];
-            mx = max(mx, cnt);
+        int n=nums.size();
+        if (n==1) return 0;
+        nums.insert(nums.end(),nums.begin(),nums.end());
+        int c=0;
+        for(int i=0;i<n;i++) if (nums[i]==1) c+=1;
+        int i=0;
+        int z=0;
+        int j=c-1;
+        int m=n-c;
+        for(int x=0;x<c;x++) if (nums[x]==0) z+=1;
+        cout<<z;
+        for(i=0;i<n;i++){
+            if (nums[i]==0) z-=1;
+            if (nums[j+1]==0) z+=1;
+            j+=1;
+            m=min(z,m);
         }
-        return k - mx;
+        return m;
     }
 };
