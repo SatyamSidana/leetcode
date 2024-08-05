0c4ec8cf7fd0caaeca49ef9e14eee31efd795587
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-08-05 17:41:59 +0530

    Time: 788 ms (9.02%) | Memory: 22.1 MB (17.34%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.py
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.py
@@ -1,8 +1,20 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
-        ans, countB = 0, 0
-        for ch in s:
-            if ch == 'a':
-                ans = min(ans+1, countB)
-            else: countB += 1
-        return ans
+        n = len(s)
+        prefix_b = [0]
+        suffix_a = [0] * n
+        for i in range(1, n):
+            if s[i - 1] == 'b':
+                prefix_b.append(prefix_b[-1] + 1)
+            else:
+                prefix_b.append(prefix_b[-1])
+        for i in range(n - 2, -1, -1):
+            if s[i + 1] == 'a':
+                suffix_a[i] = suffix_a[i + 1] + 1
+            else:
+                suffix_a[i] = suffix_a[i + 1]
+    
+        res = inf
+        for pivot in range(n):
+            res = min(res, prefix_b[pivot] + suffix_a[pivot])
+        return res
