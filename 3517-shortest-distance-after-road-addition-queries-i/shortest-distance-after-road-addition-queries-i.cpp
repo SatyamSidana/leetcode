0b6e7706d89520b91201f8938e348d9ac97ea373
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-08-05 16:12:35 +0530

    Time: 584 ms (14.29%) | Memory: 144.5 MB (14.29%) - LeetSync

--- 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.cpp
+++ 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.cpp
@@ -1,55 +1,33 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <climits>
-
-using namespace std;
-
 class Solution {
 public:
-    // DFS-like shortest path using priority queue (Dijkstra's algorithm-like)
-    int dfs(int n, vector<vector<int>>& ad) {
-        // Min-heap priority queue to find shortest paths
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        vector<int> v(n, INT_MAX);  // Distance vector, initially set to "infinity"
-        
-        // Starting node with distance 0
-        v[0] = 0;
-        pq.push({0, v[0]});
-
-        while (!pq.empty()) {
-            int x = pq.top().first;  // Current node
-            int y = pq.top().second; // Current distance
+    int dfs(int n, vector<vector<int>>& ad){
+        priority_queue<pair<int,int> ,vector<pair<int,int>> ,greater<pair<int,int>>>pq;
+        vector<int>v(n,INT_MAX);
+        v[0]=0;
+        pq.push({0,v[0]});
+        while(!pq.empty()){
+            int x=pq.top().first;
+            int y=pq.top().second;
             pq.pop();
-
-            // Iterate over all neighbors of node x
-            for (auto i : ad[x]) {
-                if (y + 1 < v[i]) {  // If a shorter path to node i is found
-                    v[i] = y + 1;
-                    pq.push({i, y + 1});
+            for(auto i:ad[x]){
+                if (y+1<v[i]){
+                    v[i]=y+1;
+                    pq.push({i,y+1});
                 }
             }
         }
-
-        return v[n - 1];  // Return the distance to the last node
+        return v[n-1];
     }
-
     vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& q) {
-        vector<vector<int>> a(n);  // Initialize the adjacency list with size n
-        vector<int> b;
-
-        // Construct the initial adjacency list with sequential edges
-        for (int i = 0; i < n - 1; i++) {
-            a[i].push_back(i + 1);
+        vector<vector<int>>a(n);
+        vector<int>b;
+        for(int i=0;i<n-1;i++){
+            a[i].push_back(i+1);
         }
-
-        // Process each query
-        for (int i = 0; i < q.size(); i++) {
-            a[q[i][0]].push_back(q[i][1]);  // Add the edge defined by the query
-            int shortestDist = dfs(n, a);    // Debug: print the shortest distance
-            b.push_back(shortestDist);      // Store the result
+        for(int i=0;i<q.size();i++){
+            a[q[i][0]].push_back(q[i][1]);
+            b.push_back(dfs(n,a));
         }
-
         return b;
     }
 };
