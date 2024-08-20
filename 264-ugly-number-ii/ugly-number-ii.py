7bf0646a4df8845cc20b5bfcf4a932fc87f40646
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-08-20 19:46:30 +0530

    Time: 2075 ms (5.01%) | Memory: 17 MB (6.08%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.py
+++ 264-ugly-number-ii/ugly-number-ii.py
@@ -4,8 +4,7 @@ class Solution:
         for i in range(33) : 
             for j in range(22):
                 for k in range(17):
-                    if (2*i + 3*j + 4*k < 66):
-                        ans.append((2**i) * (3**j) * (5**k))
+                    ans.append((2**i) * (3**j) * (5**k))
         ans.sort()
         return ans[n-1]
         
