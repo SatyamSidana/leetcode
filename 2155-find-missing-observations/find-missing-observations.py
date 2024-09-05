567aa447977806b1ea527d48e0d2991b280574f6
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-09-06 02:31:26 +0530

    Time: 1041 ms (26.51%) | Memory: 27.5 MB (27.53%) - LeetSync

--- 2155-find-missing-observations/find-missing-observations.py
+++ 2155-find-missing-observations/find-missing-observations.py
@@ -2,11 +2,18 @@ class Solution:
     def missingRolls(self, r: List[int], mean: int, n: int) -> List[int]:
         m=sum(r)
         need=mean*(n+len(r))-m
+        print(need)
         ans=[1]*n
         need-=n
         if need<0:
             return []
-        c=1
+        x=(need+n)//n
+        ans=[x]*n
+        for i in range(need%n):
+            ans[i]+=1
+        if ans[0]>6:
+            return []
+        return ans
         while need>0 and c<6:
             for i in range (min(need,n)):
                 ans[i]+=1
