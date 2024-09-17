873cbdda1515931c49e3f4d4e663e528acc2ce8f
SatyamSidana <97503180+SatyamSidana@users.noreply.github.com>
2024-09-18 00:07:47 +0530

    Time: 33 ms (74.64%) | Memory: 16.5 MB (44.21%) - LeetSync

--- 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.py
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.py
@@ -1,27 +1,13 @@
 class Solution:
     def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
-        a=s1.split()
-        b=s2.split()
+        a=s1.split()+s2.split()
         d1={}
-        d2={}
         ans=[]
         for i in a:
             if i in d1:
                 d1[i]=-1
             else:
                 d1[i]=1
-        for i in b:
-            if i in d2:
-                d2[i]=-1
-            else:
-                d2[i]=1
-        for i in d1:
-            if i in d2:
-                d1[i]=-1
-                d2[i]=-1
-        for i in d2:
-            if d2[i]==1:
-                ans.append(i)
         for i in d1:
             if d1[i]==1:
                 ans.append(i)
